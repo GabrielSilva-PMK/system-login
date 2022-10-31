b0a82670fbd4404ca8b257c1a5c414d00240856f
“GabrielSilva-PMK” <gabriel.silva@pmk.com.br>
2022-10-31 17:44:06 -0300

    update controller login -> autentication

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,62 +35,52 @@ class LoginController extends Controller
     public function autentication(Request $request)
     {
 
+        Log::debug("autentication");
 
-        $credentials = ['email' => $request->email, 'password' => $request->password];
-
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-
-            return redirect()->intended('home');
-        }
-
-        return back()->withErrors([
-            'teste' => 'Usuário ou Senha incorreto!',
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
         ]);
+        $head = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ];
+        $bodyRequest = $request->only(['email', 'password']);
+        $body = json_encode($bodyRequest);
 
-        // Log::debug("autentication");
+        $credentials = [];
 
-        // $request->validate([
-        //     'email' => 'required',
-        //     'password' => 'required'
-        // ]);
-        // $head = [
-        //     'Content-Type' => 'application/json',
-        //     'Accept' => 'application/json'
-        // ];
-        // $bodyRequest = $request->only(['email', 'password']);
-        // $body = json_encode($bodyRequest);
+        try {
 
-        // $credentials = [];
+            $credentials = ['email' => $request->email, 'password' => $request->password];
 
-        // Log::debug("GABRIEL");
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
 
-        // try {
-        //     // $client = new Client();
-        //     // $requestUser = new RequestGuzzle('POST', env('END_POINT_BACKEND') . '/api/login/user', $head, $body);
-        //     // $promiseCredentials = $client->sendAsync($requestUser)->then(function ($response) {
-        //     //     return json_decode($response->getBody()->getContents(),true);
-        //     // });
-        //     // $credentials = $promiseCredentials->wait();
+                // return redirect()->intended('home');
+                return redirect(route('home'));
+            }else{
+                // return back()->withErrors([
+                //     'errors' => 'Usuário ou Senha incorreto!',
+                // ]);
 
-        //     $teste = AuthController::class;
+                // return back()
+                //         ->withErrors(['message' => 'Email ou Senha incorreto!'])
+                //         ->withInput();
 
-        //     Log::debug($credentials);
+                return redirect()->back()->with('errors', 'Email ou Senha incorreto!');
+            }
 
-        //     Log::debug("OPA");
-
-        //     return redirect(route('home'));
-
-        // } catch (Exception $e) {
-        //     Session::put('fail', 'Email ou senha incorreto');
-        //     return view('auth.login', ['credential' => $credentials]);
-        // }
-        // $tokenController = new TokenController;
-        // $tokenController->setCredentials($credentials);
-        // $expires_in = Carbon::now()->addSeconds(($credentials['expires_in'] - env('SESSION_EXPIRE')))->format('Y-m-d H:i:s');
-        // Session::put('expires_in', $expires_in);
-        // Log::debug("PASSOU AQUI");
-        // return redirect(route('home.index'));
+        } catch (Exception $e) {
+            Session::put('fail', 'Email ou senha incorreto');
+            return view('auth.login', ['credential' => $credentials]);
+        }
+        $tokenController = new TokenController;
+        $tokenController->setCredentials($credentials);
+        $expires_in = Carbon::now()->addSeconds(($credentials['expires_in'] - env('SESSION_EXPIRE')))->format('Y-m-d H:i:s');
+        Session::put('expires_in', $expires_in);
+        Log::debug("PASSOU AQUI");
+        return redirect(route('home.index'));
     }
 
     public static function logout()
