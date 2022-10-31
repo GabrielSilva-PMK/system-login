76946e3bb85aed422a2df5532e3fb43622febdff
“GabrielSilva-PMK” <gabriel.silva@pmk.com.br>
2022-10-31 17:42:27 -0300

    update routes web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\UserAdminController;
+use App\Http\Controllers\Admin\MenuAdminController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,6 +33,22 @@ Route::get('/sessionexpire', [HomeController::class, 'sessionexpire'])->name('ho
 
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-    // Route::resource('/book', BookController::class);
+    // Route::resource('/user', UserController::class);
+
+    // UserAdminController
+    Route::get('/user',  [UserAdminController::class, 'index'])->name('user.index');
+    Route::get('/user/create',  [UserAdminController::class, 'create'])->name('user.create');
+    // Route::post('/user',  [UserAdminController::class, 'store'])->name('user.store');
+    Route::get('/user/{id}',  [UserAdminController::class, 'edit'])->name('user.edit');
+    // Route::put('/user/{id}',  [UserAdminController::class, 'update'])->name('user.update');
+    Route::delete('/user/{id}',  [UserAdminController::class, 'destroy'])->name('user.destroy');
+
+    // MenuAdminController
+    Route::get('/menu',  [MenuAdminController::class, 'index'])->name('menu.index');
+    Route::get('/menu/create',  [MenuAdminController::class, 'create'])->name('menu.create');
+    // Route::post('/menu',  [MenuAdminController::class, 'store'])->name('menu.store');
+    Route::get('/menu/{id}',  [MenuAdminController::class, 'edit'])->name('menu.edit');
+    // Route::put('/menu/{id}',  [MenuAdminController::class, 'update'])->name('menu.update');
+    Route::delete('/menu/{id}',  [MenuAdminController::class, 'destroy'])->name('menu.destroy');
 
 // });
