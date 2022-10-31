d143e45592a12d72178eccd529d524b2264e586c
“GabrielSilva-PMK” <gabriel.silva@pmk.com.br>
2022-10-31 17:43:21 -0300

    update login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,66 +2,6 @@
 
 @section('content')
 
-<style>
-
-    body{
-        margin: 0;
-        font-size: .9rem;
-        font-weight: 400;
-        line-height: 1.6;
-        color: #212529;
-        text-align: left;
-        background-color: #f5f8fa;
-    }
-
-    .navbar-laravel
-    {
-        box-shadow: 0 2px 4px rgba(0,0,0,.04);
-    }
-
-    .navbar-brand , .nav-link, .my-form, .login-form
-    {
-        font-family: Raleway, sans-serif;
-    }
-
-    .my-form
-    {
-        padding-top: 1.5rem;
-        padding-bottom: 1.5rem;
-    }
-
-    .my-form .row
-    {
-        margin-left: 0;
-        margin-right: 0;
-    }
-
-    .login-form
-    {
-        padding-top: 1.5rem;
-        padding-bottom: 1.5rem;
-    }
-
-    .login-form .row
-    {
-        margin-left: 0;
-        margin-right: 0;
-    }
-</style>
-
-
-<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
-    <div class="container">
-        <a class="navbar-brand">Sistema Bibliotecário</a>
-    </div>
-</nav>
-
-@error('teste')
-    <span class="invalid-feedback" role="alert">
-        <strong>{{ $message }}</strong>
-    </span>
-@enderror
-
 <main class="login-form">
     <div class="cotainer">
         <div class="row justify-content-center">
@@ -69,31 +9,41 @@
                 <div class="card">
                     <div class="card-header">Login</div>
                         <div class="card-body">
-                        <!-- <form id="formLogin" name="formLogin"> -->
-                        <form action="{{route('login-autentication')}}" method="POST">
-                            {{ csrf_field() }}
-                            @csrf
-                            @method('POST')
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="email" class="form-control" name="email" required autofocus>
-                                </div>
-                            </div>
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-                                <div class="col-md-6">
-                                    <input type="password" id="password" class="form-control" name="password" required>
+                            @if (\Session::has('errors'))
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <strong>{!! \Session::get('errors') !!}</strong>
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                                        <span aria-hidden=true>&times;</span>
+                                    </button>
                                 </div>
-                            </div>
+                            @endif
 
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Enter
-                                </button>
-                            </div>
-                        </form>
+                            <!-- <form id="formLogin" name="formLogin"> -->
+                            <form action="{{route('login-autentication')}}" method="POST">
+                                {{ csrf_field() }}
+                                @csrf
+                                @method('POST')
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail</label>
+                                    <div class="col-md-6">
+                                        <input type="text" id="email" class="form-control" name="email" required autofocus>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                                    <div class="col-md-6">
+                                        <input type="password" id="password" class="form-control" name="password" required>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Enter
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                 </div>
             </div>
